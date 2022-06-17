36d7ec7c55850907ec7693ed32f22c928e2e4b85
Sange0104 <sange.pp@gmail.com>
2022-06-17 16:10:38 -0400

    First commit to the branch

--- src/test/java/PageObjectCloth/LoginPage.java
+++ src/test/java/PageObjectCloth/LoginPage.java
@@ -1,5 +1,12 @@
 package PageObjectCloth;
 
-public class LoginPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+public class LoginPage {
+WebDriver driver;
+
+	@FindBy(id = "login")
+	WebElement login;
 }
